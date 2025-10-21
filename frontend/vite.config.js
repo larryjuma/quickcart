2b5f832c04d470718877dd48b156334e146c1b51
larryjuma <larryjuma198@gmail.com>
2025-10-21 18:20:04 +0300

    Fix image paths for gh-pages

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/quickcart-frontend/', // 👈 important for GitHub Pages
+  base: '/quickcart-frontend/', // ✅ Must match your GitHub repo name
 })
